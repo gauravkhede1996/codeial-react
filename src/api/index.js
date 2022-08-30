877f623b86adfc758b950a68f46355c3dcfd39ce
Gaurav khede <gauravk@propelapps.com>
2022-08-31 01:26:10 +0530

    done till customFetch II functions

---
+++ src/api/index.js
@@ -0,0 +1,55 @@
+import {LOCALSTORAGE_TOKEN_KEY,API_URLS} from '../utils/index';
+const customFetch=async (url,{body,...customConfig})=>{
+    const token= window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+    const headers={
+        "mode":"no-cors",
+        'content-type':'application/json',
+        Accept:'application/json'
+    }
+    if(token){
+        headers.Authorization=`Bearer ${token}`;
+    }
+    const config={
+        ...customConfig,
+        headers:{
+            ...headers,
+            ...customConfig.headers
+        }
+    }
+    if(body){
+        config.body=JSON.stringify(body);
+    }
+    try{
+        console.log(fetch(url,config),' is the result of fetch');
+        const response=await fetch(url,config);
+        console.log(response,' is the response');
+        const data=await response.json();
+        console.log(data,' is the data');
+        if(data.success){
+            return{
+                data:data.data,
+                success:true
+            }
+        }
+        throw new Error(data.message);
+    }catch(error){
+        console.log(error);
+        return{
+            message:error.message,
+            success:false
+        };
+    }
+}
+// const namely=(name)=> `https://agify.io/?name=${name}`;
+export const getPosts= (page=1,limit=5)=>{
+    return customFetch(API_URLS.posts(page,limit),{
+        method:'GET',
+        
+    })
+}
+// export const getName=(name='gaurav')=>{
+//     return customFetch(namely(name),{
+//         method:'GET',
+//         mode:'no-cors'
+//     })
+// }
